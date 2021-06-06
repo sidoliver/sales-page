2bf4afc509a12acb4705dbdc565a5d3f94d3a379
sidoliver <sidchoice@gmail.com>
2021-06-06 17:31:27 +0530

    mobile responsive navbar

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const btns = document.querySelectorAll('.form-btn')
 const form = document.querySelector('.form-container')
 const closeBtn = document.querySelector('.close-btn')
+const closeMenu = document.querySelector('.close')
+const burger = document.querySelector('.burger')
+const navMenu = document.querySelector('.nav-list')
 console.log(btns)
 btns.forEach(btn => {
     btn.addEventListener('click',()=>{
@@ -9,4 +12,15 @@ btns.forEach(btn => {
 })
 closeBtn.addEventListener('click',()=> {
     form.classList.remove('show-form')
+})
+
+burger.addEventListener('click',() =>{
+    navMenu.style.display = 'flex'
+    burger.style.display = 'none'
+    closeMenu.classList.add('close-show')
+})
+closeMenu.addEventListener('click',()=> {
+    closeMenu.classList.remove('close-show')
+    burger.style.display = 'block'
+    navMenu.style.display = 'none'
 })
